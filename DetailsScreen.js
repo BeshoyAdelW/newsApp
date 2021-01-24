75f1c6621af93ce0ff7323b76df06d6e71e62a1f
Beshoy Adel <Beshoy.adelwaheeb@yahoo.com>
2021-01-25 01:27:12 +0200

    Built list screen

---
+++ DetailsScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+function DetailsScreen(props) {
+  return <WebView url={this.state.url} />;
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
+export default DetailsScreen;
---
+++ src/api/news.js
@@ -0,0 +1,15 @@
+import { create } from "apisauce";
+
+const api = create({
+  baseURL: "http://newsapi.org/v2/top-headlines",
+});
+
+const getNews = () => {
+  const ApiKey = "576d030b0d794182923fdc2ae28e433d";
+  let url = `?country=eg&apiKey=${ApiKey}`;
+  return api.get(url);
+};
+
+export default {
+  getNews,
+};
